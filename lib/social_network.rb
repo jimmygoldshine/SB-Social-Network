abc31e6cb3f968bc8e48107e34d892f70690598e
James <jamesdix@cwgsy.net>
2017-04-18 12:16:10 +0100

    all users changed to hash key=name value=user_profile_object

--- lib/social_network.rb
+++ lib/social_network.rb
@@ -1,7 +1,11 @@
 class SocialNetwork
 
+  def initialize(user_profile_class)
+    @user_profile = user_profile_class.new
+  end
+
   def all_users
-    ["Elon Musk", "Jeff Bezos", "Mark Zuckerberg","Peter Thiel", "Reid Hoffman"]
+    {:"Elon Musk" => @user_profile, :"Jeff Bezos" => @user_profile, :"Mark Zuckerberg" => @user_profile, :"Peter Thiel" => @user_profile, :"Reid Hoffman" => @user_profile}
   end
 
 end
--- spec/social_network_spec.rb
+++ spec/social_network_spec.rb
@@ -2,12 +2,22 @@ require 'spec_helper'
 
 describe SocialNetwork do
 
-  let(:network) {described_class.new}
+  let(:user_profile_class) {double(:user_profile_class)}
+  let(:network) {described_class.new(user_profile_class)}
+  let(:user_profile) { double(:user_profile) }
+
+  before do
+    allow(user_profile_class).to receive(:new).and_return(user_profile)
+  end
 
   describe 'Founding Members' do
 
-    it "should have five user profiles created upon initialization" do
+    it "should have five users created upon initialization" do
       expect(network.all_users.length).to eq(5)
+    end
+
+    it "should have a profile associated with their names" do
+      expect(network.all_users[:"Elon Musk"]).to be(user_profile)
     end
 
   end
