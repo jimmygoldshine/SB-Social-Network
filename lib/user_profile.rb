71aaa970dd5e18efc3b98e0f0d76f5335d9d3e25
James <jamesdix@cwgsy.net>
2017-04-18 14:34:18 +0100

    successfully stubbed user input for profile name

--- lib/user_profile.rb
+++ lib/user_profile.rb
@@ -1,11 +1,19 @@
 class UserProfile
 
+  attr_reader :name, :friends
+
   def initialize
+    @name = get_name
     @friends = {}
+
   end
 
-  def friends
-    @friends
+  def get_name
+    puts "First Name: "
+    first_name = gets.chomp
+    puts puts "Last Name: "
+    last_name = gets.chomp
+    @name = "#{first_name} #{last_name}"
   end
 
 end
--- spec/user_profile_spec.rb
+++ spec/user_profile_spec.rb
@@ -4,12 +4,16 @@ describe UserProfile do
 
   let(:profile) {described_class.new}
 
-  describe 'New Profile' do
+  before do
+    allow_any_instance_of(UserProfile).to receive(:gets).and_return("James","Dix")
+  end
 
-    it "should have a friends list of 0" do
-      expect(profile.friends.length).to eq(0)
-    end
+  it "should have a friends list of 0" do
+    expect(profile.friends.length).to eq(0)
+  end
 
+  it "should be initialized with a First Name and Last Name" do
+    expect(profile.name).to eq("James Dix")
   end
 
 end
