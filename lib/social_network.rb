542cba1053621207657809d889dbc3658d568917
James <jamesdix@cwgsy.net>
2017-04-20 11:02:24 +0100

    refactor of user_profile

--- lib/social_network.rb
+++ lib/social_network.rb
@@ -1,6 +1,6 @@
 class SocialNetwork
 
-  attr_reader :all_users
+  attr_reader :all_users, :user_profile_class, :message_class
 
   def initialize(user_profile_class, message_class)
     @user_profile_class = user_profile_class
@@ -9,15 +9,15 @@ class SocialNetwork
   end
 
   def sign_up
-    profile = @user_profile_class.new(self, @message_class)
+    profile = user_profile_class.new(self, message_class)
     user_name = profile.name
-    @all_users[user_name.to_sym] = profile
+    all_users[user_name.to_sym] = profile
   end
 
   def show_all_users
     user_count = 0
-    puts "There are #{@all_users.length} users on the SB-Social-Network: \n"
-    @all_users.each_key do |name|
+    puts "There are #{all_users.length} users on the SB-Social-Network: \n"
+    all_users.each_key do |name|
       user_count += 1
       puts "#{user_count}. #{name}"
     end
--- lib/user_profile.rb
+++ lib/user_profile.rb
@@ -1,6 +1,6 @@
 class UserProfile
 
-  attr_reader :name, :friends, :unread_messages
+  attr_reader :name, :friends, :unread_messages, :read_messages, :all_users, :message_class
 
   def initialize(network, message_class)
     @name = set_name
@@ -12,41 +12,41 @@ class UserProfile
   end
 
   def add_friend
-    puts "\n#{@name}, you would like to add a friend... what's their full name?"
+    puts "\n#{name}, you would like to add a friend... what's their full name?"
     friends_name = gets.chomp
-    if @all_users[friends_name.to_sym]
-      friends_profile = @all_users[friends_name.to_sym]
-      @friends[friends_name.to_sym] = friends_profile
+    if user_exists?
+      friends_profile = all_users[friends_name.to_sym]
+      friends[friends_name.to_sym] = friends_profile
       puts "\nWe successfully found #{friends_name}! You are now friends - go say hi! \n"
     else
       puts "No users found with that name. Are you sure they have a profile?!"
-      self.add_friend
+      try_again(add_friend)
     end
   end
 
   def send_message
-    message = @message_class.new(self)
+    message = message_class.new(self)
     message.send
   end
 
 
   def check_messages
     if any_unread_messages?
-      puts "\n#{@name}, you have #{@unread_messages.length} new message#{pluralize}: \n"
-      @unread_messages.each do |message|
+      puts "\n#{name}, you have #{unread_messages.length} new message#{pluralize}: \n"
+      unread_messages.each do |message|
         puts "#{message.read} \n\n"
       end
-      @read_messages << @unread_messages
-      @unread_messages.clear
+      read_messages << unread_messages
+      unread_messages.clear
     else
-      puts "\n #{@name}, you have 0 new messages"
+      puts "\n #{name}, you have 0 new messages"
     end
   end
 
   private
 
   def pluralize
-    return @unread_messages.length != 1 ? "s" : ""
+    return unread_messages.length != 1 ? "s" : ""
   end
 
   def set_name
@@ -55,13 +55,21 @@ class UserProfile
     first_name = gets.chomp
     puts "Last Name: "
     last_name = gets.chomp
-    @name = "#{first_name} #{last_name}"
+    name = "#{first_name} #{last_name}"
     puts "Thank you for signing up, #{first_name}!\n\n"
-    @name
+    name
   end
 
   def any_unread_messages?
-    @unread_messages.any?
+    unread_messages.any?
+  end
+
+  def user_exists?
+    all_users[friends_name.to_sym]
+  end
+
+  def try_again(method)
+    self.method
   end
 
 end
